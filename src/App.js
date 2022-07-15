c54e14a84c333b7c2c36e309c043b88b6c976e5c
JohnSmithW <55023917+JohnSmithW@users.noreply.github.com>
2022-07-15 11:02:52 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import './scss/main.scss';
 import UsersTable from './components/UsersTable/UsersTable';
 
 export default function App() {
   return (
+     <Provider store={store}>
     <div className="main">
       <UsersTable />
     </div>
+ </Provider>
   );
 }
