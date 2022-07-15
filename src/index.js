27fbcf5f06326a65bda7fe7a0d655b7628f052cd
JohnSmithW <55023917+JohnSmithW@users.noreply.github.com>
2022-07-15 10:53:27 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 
-const root = document.createElement('div');
-root.id = 'root';
-document.body.appendChild(root);
+// const root = document.createElement('div');
+// root.id = 'root';
+// document.body.appendChild(root);
 
 ReactDom.render(
   <Provider store={store}>
